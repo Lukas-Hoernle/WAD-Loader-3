6682548aa3a5931cab9307aba6aa539c60b74df2
maxi <maximilian.lincks@gmx.net>
2024-04-03 10:12:25 +0200

    added initial spring project

---
+++ wadloader3/src/test/java/com/luma/wadloader3/Wadloader3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luma.wadloader3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wadloader3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
