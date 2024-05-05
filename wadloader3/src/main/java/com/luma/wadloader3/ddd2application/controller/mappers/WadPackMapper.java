8822c6768eba5e0f6bca117fa01c6877c02b846b
Maxi <maximilian.lincks@gmx.net>
2024-05-05 22:50:48 +0200

    implemented file downloads

--- wadloader3/src/main/java/com/luma/wadloader3/ddd2application/controller/mappers/WadPackMapper.java
+++ wadloader3/src/main/java/com/luma/wadloader3/ddd2application/controller/mappers/WadPackMapper.java
@@ -6,7 +6,6 @@ import com.luma.wadloader3api.model.WadPackDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
