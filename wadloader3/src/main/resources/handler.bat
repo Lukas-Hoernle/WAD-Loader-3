239630aa741c01052afb75c9813e96270c68c080
Maxi <maximilian.lincks@gmx.net>
2024-05-14 23:43:45 +0200

    cleanup

--- wadloader3/src/main/resources/handler.bat
+++
@@ -1,7 +0,0 @@
-REM just echo the passed argument
-REM PLACEHOLDER HANDLER, TODO REPLACE WITH HASKELL HANDLER
-echo off
-echo Hello from the custom protocol handling script.
-echo script name: %0
-echo script arg : %1
-pause
