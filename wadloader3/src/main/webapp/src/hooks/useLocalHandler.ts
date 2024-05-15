b324ba8a3240a59ea05ed348fb9821f3ebaff1f7
Maxi <maximilian.lincks@gmx.net>
2024-05-15 20:59:34 +0200

    added use local handler hook

---
+++ wadloader3/src/main/webapp/src/hooks/useLocalHandler.ts
@@ -0,0 +1,14 @@
+type WadpackOption = "startwadpack" | "wadpack";
+/**
+ * only works on positive integers
+ */
+type WadpackId = number;
+/**
+ * only works on positive integers
+ */
+type WadId = number;
+
+export function useLocalHandler(serverUrl: string) {
+  return (option: WadpackOption, packId: WadpackId, wads: [WadId]) =>
+    window.open(`wadloader://${option}${packId}-${wads.join('-')}$${serverUrl}`);
+}
