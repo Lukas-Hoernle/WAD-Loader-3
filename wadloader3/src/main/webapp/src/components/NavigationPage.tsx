e3dda74d1e85996c8720a6dd479d674b1e184199
Lukas <lukas.hoernle@web.de>
2024-06-23 16:36:38 +0200

    rework navigation

--- wadloader3/src/main/webapp/src/components/NavigationPage.tsx
+++ wadloader3/src/main/webapp/src/components/NavigationPage.tsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 
 const NavigationPage = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            WAD Loader 3
-          </Typography>
-          <Button component={Link} to="/createwad" color="inherit">
-            Create Wad
-          </Button>
-          <Button component={Link} to="/createwadpack" color="inherit">
-            Create WadPack
-          </Button>
-          <Button component={Link} to="/wadlist" color="inherit">
-            Wad List
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          WAD Loader 3
+        </Typography>
+        <Button component={Link} to="/createwad" color="inherit">
+          Create Wad
+        </Button>
+        <Button component={Link} to="/createwadpack" color="inherit">
+          Create WadPack
+        </Button>
+        <Button component={Link} to="/wadlist" color="inherit">
+          Wad List
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 };
 
--- wadloader3/src/main/webapp/src/components/router.tsx
+++ wadloader3/src/main/webapp/src/components/router.tsx
@@ -1,27 +1,32 @@
-import { useNavigate } from 'react-router-dom';
-import { Button, Box } from '@mui/material';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavigationPage from './NavigationPage';
+import CreateWad from './CreateWad';
+import CreateWadPack from './CreateWadPack';
+import WadList from './WadList';
 
-function MainLayout() {
-  const navigate = useNavigate();
-
-  const navigateToCreateWad = () => {
-    navigate('/createwad');
-  };
-
-  const navigateToCreateWadPack = () => {
-    navigate('/createwadpack');
-  };
-
+function App() {
   return (
-    <Box display="flex" flexDirection="column" alignItems="center" mt={5}>
-      <Button variant="contained" onClick={navigateToCreateWad}>
-        Create Wad
-      </Button>
-      <Button variant="contained" onClick={navigateToCreateWadPack} mt={2}>
-        Create WadPack
-      </Button>
-    </Box>
+    <Router>
+      <NavigationPage />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/createwad" element={<CreateWad />} />
+        <Route path="/createwadpack" element={<CreateWadPack />} />
+        <Route path="/wadlist" element={<WadList />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default MainLayout;
+function HomePage() {
+  return (
+    <div>
+      <h1>Auswahl text</h1>
+      <p>siuuuu.</p>
+    </div>
+  );
+}
+
+export default App;
