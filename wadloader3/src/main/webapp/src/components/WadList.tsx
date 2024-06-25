ce8ee1515e6305c77fef0b0f6f2bedd74a8ba7de
Lukas <lukas.hoernle@web.de>
2024-06-25 13:36:58 +0200

    fix downloadwad

--- wadloader3/src/main/webapp/src/components/WadList.tsx
+++ wadloader3/src/main/webapp/src/components/WadList.tsx
@@ -15,14 +15,14 @@ function WadList() {
         fetchWads();
     }, [wadApi]);
 
-    const handleDownload = async (wadId: number, fileExtension: string) => {
+    const handleDownload = async (wadId: number) => {
         const getWadRequest: GetWadRequest = { id: wadId };
         const wad = await wadApi.getWad(getWadRequest);
         const wadBlob = new Blob([JSON.stringify(wad)], { type: 'application/octet-stream' }); 
         const url = window.URL.createObjectURL(wadBlob);
         const link = document.createElement('a');
         link.href = url;
-        link.setAttribute('download', `${wad.name}.${fileExtension}`); 
+        link.setAttribute('download', wad.name);
         document.body.appendChild(link);
         link.click();
         link.parentNode?.removeChild(link);
@@ -35,11 +35,8 @@ function WadList() {
                 {wads.map((wad) => (
                     <ListItem key={wad.id}>
                         <ListItemText primary={wad.name} secondary={wad.description} />
-                        <Button variant="contained" color="primary" onClick={() => handleDownload(wad.id, 'wad')}>
-                            Download as WAD
-                        </Button>
-                        <Button variant="contained" color="secondary" onClick={() => handleDownload(wad.id, 'pk3')}>
-                            Download as PK3
+                        <Button variant="contained" color="primary" onClick={() => handleDownload(wad.id)}>
+                            Download
                         </Button>
                     </ListItem>
                 ))}
