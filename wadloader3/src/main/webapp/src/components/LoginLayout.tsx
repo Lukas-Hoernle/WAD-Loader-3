2770cb3c2b48bf74c4f17b23c3ef384b6f014343
Lukas <lukas.hoernle@web.de>
2024-06-25 21:54:37 +0200

    remnove duplicated logout

--- wadloader3/src/main/webapp/src/components/LoginLayout.tsx
+++ wadloader3/src/main/webapp/src/components/LoginLayout.tsx
@@ -1,11 +1,9 @@
 import { Button } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { login } from "../hooks/useLogin";
-import { useLogout } from "../hooks/useLogout";
 import { useUser } from "../hooks/useUser";
 
 export function LoginLayout() {
-  const logout = useLogout();
   const { loading, authenticated } = useUser();
 
   if (loading) {
@@ -15,7 +13,6 @@ export function LoginLayout() {
   if (authenticated) {
     return (
       <>
-        <Button onClick={logout}>Logout</Button>
         <Outlet></Outlet>
       </>
     );
