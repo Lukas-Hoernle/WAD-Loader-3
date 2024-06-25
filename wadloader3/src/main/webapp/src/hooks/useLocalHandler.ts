d269cc3b0e659dfbc8f812b1c3d309723983866f
Lukas.Hoernle <Lukas.Hoernle@cas.de>
2024-06-25 08:40:02 +0200

    comment

--- wadloader3/src/main/webapp/src/hooks/useLocalHandler.ts
+++ wadloader3/src/main/webapp/src/hooks/useLocalHandler.ts
@@ -8,7 +8,7 @@ type WadpackId = number;
  */
 type WadId = number;
 
-export function useLocalHandler(serverUrl: string) {
+export function useLocalHandler(serverUrl: string) {//localhost 8080 hardcoden als serverurl
   return (option: WadpackOption, packId: WadpackId, wads: [WadId]) =>
     window.open(`wadloader://${option}${packId}-${wads.join('-')}$${serverUrl}`);
 }
